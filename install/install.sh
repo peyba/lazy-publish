0b1c50462a3afd0a5f8944ee75b26adcb8233416
pererushev <pererushev@gmail.com>
2022-07-11 15:45:06 +0300

    repair installer

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp lazyp-completion.bash /etc/bash_completion.d/lazyp-completion.bash
+cp ../lazyp-completion.bash /etc/bash_completion.d/lazyp-completion.bash
 chmod 644 /etc/bash_completion.d/lazyp-completion.bash
-install lazyp /bin/lazyp
+install ../lazyp /bin/lazyp
